4300d413556baf959a0e6784f6c655cebe0beba8
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2020-06-15 10:33:41 +0800

    test last commit

---
+++ Lab10 - MongoDB II/services/dataservice.js
@@ -0,0 +1,81 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+var eventSchema = {};
+var eventModel;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.set('debug', true);
+
+var database = {
+    connect: function() {
+        mongoose.connect('mongodb://localhost:27017/eventsDB', function(err){
+            if(err==null) {
+                console.log("Connected to Mongo DB");
+                //initialize values
+                eventSchema = schema({
+                    name: String,
+                    description: String,
+                    start: {
+                        date: String,
+                        time: String
+                    },
+                    end: {
+                        date: String,
+                        time: String
+                    }
+                });
+                var connection = mongoose.connection;
+                eventModel = connection.model('Event', eventSchema);
+            } else {
+                console.log("Error connecting to Mongo DB");
+            }
+        })
+    },
+    addEvent: function(n, d, sd, st, ed, et, callback) {
+        var newEvent = new eventModel({
+            name: n,
+            description: d,
+            start: {
+                date: sd,
+                time: st
+            },
+            end: {
+                date: ed,
+                time: et
+            }
+        });
+        newEvent.save(callback);
+    },
+    getAllEvents: function(callback) {
+        eventModel.find({},callback);
+    },
+    getEvent: function(id, callback) {
+        eventModel.findById(id,callback);
+    },
+    updateEvent: function(id, n, d, sd, st, ed, et,callback) {
+        var updatedEvent = {
+            name: n,
+            description: d,
+            start: {
+                date: sd,
+                time: st
+            },
+            end: {
+                date: ed,
+                time: et
+            }
+        };
+        eventModel.findByIdAndUpdate(id, updatedEvent, callback);
+    },
+    deleteEvent: function(id,callback) {
+        eventModel.findByIdAndDelete(id,callback);
+    }
+
+
+};
+
+module.exports = database;
