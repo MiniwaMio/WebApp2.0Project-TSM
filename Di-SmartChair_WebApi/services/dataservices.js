c521477757b1cae70e44ca97ef0eaeeaa91bd518
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2020-07-05 19:31:47 +0800

    Create dataservices.js
    create and added some dataservice code to the file. Note :  note completed

---
+++ Di-SmartChair_WebApi/services/dataservices.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+var recordSchema = {};
+var recordModel;
+var userSchema = {};
+var userModel;
+var settingSchema = {};
+var settingModel;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+var database = {
+    connect: function(){
+        mongoose.connect('mongodb://localhost:27017/DiSmartChairDB', function(err){
+            if(err = null){
+                console.log("Connected to Mongo DB");
+                //Initialised Values
+                recordSchema = schema({
+                    userId: String,
+                    duration: Number,
+                    date: Date,
+                    postureCount : Number,
+                });
+                userSchema = schema({
+                    userId: String,
+                    email : String,
+                    username: String,
+                    password : String,
+                });
+                settingSchema = schema({
+                    userid: String,
+                    status: String,
+                    strength : Number,
+                });
+                var connection = mongoose.connection;
+                recordModel = connection.model("Record", recordSchema);
+                userModel = connection.model("User", userSchema);
+                userModel = connection.model("Setting", settingSchema);
+            }else{
+                console.log("Error connecting to Mongo DB");
+            }
+        })
+    },
+    addAccount: function(uid, em, un,pw,callback){
+        var newUser = new userModel({
+            userId = uid,
+            email = em,
+            username = un,
+            password = pw
+        });
+        newUser.save(callback);
+    },
+    getAccount: function(id, callback){
+        userModel.findById(id, callback);
+    },
+    addRecord: function(uid, dura,Tdate, postC){
+        var newRecord = new recordModel({
+            userId = uid,
+            duration = dura,
+            date = Tdate,
+            postureCount = postC,
+        });
+    },
+    getRecords: function(callback){
+        recordModel.find({}, callback);
+    },
+    addSetting: function(uid, st, str,callback){
+        var newSetting = new settingModel({
+            userid = uid,
+            status = st,
+            strength = str
+        });
+        settingModel.save(callback);
+    },
+    getSetting: function(sid, uid, callback){
+        settingModel.find({_id = sid , userid = uid}, callback);
+    },
+    updateSetting: function(sid,str,stats, callback){
+        settingModel.updateOne({_id = sid}, {strength = str, status = stats}, callback);
+    },
+    deleteSetting: function(sid, callback){
+    },
+}
