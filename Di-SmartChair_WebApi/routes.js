c8df655ad352a469cb4ff817c7720ef10eba2bcc
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2020-07-12 00:52:57 +0800

    update routes, server and dataservice js
    added routes that connects to the sites

--- Di-SmartChair_WebApi/routes.js
+++ Di-SmartChair_WebApi/routes.js
@@ -4,7 +4,156 @@ var db = require('./services/dataservices.js');
 db.connect();
 
 var routes = function(){
+    var router = require('express').Router();
 
+    router.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    router.get('/css/*', function(req, res)  {
+        res.sendFile(__dirname+"/views/"+req.originalUrl);
+    });
+    
+    router.get('/js/*', function(req, res)  {
+        res.sendFile(__dirname+"/views/"+req.originalUrl);
+    });
+
+    router.get('/', function(req, res){
+        res.sendFile(__dirname + "/views/index.html");
+    });
+
+    router.get('/registration', function(req,res){
+        res.sendFile(__dirname + "/views/registration.html");
+    });
+
+    router.get('/data', function(req, res){
+        res.sendFile(__dirname + "/views/data.html");
+    });
+
+    router.get('/settings', function(req,res){
+        res.sendFile(__dirname + "/views/settings.html");
+    });
+
+    router.get('/features', function(req,res){
+        res.sendFile(__dirname + "/views/features.html");
+    });
+    
+    router.get('/feedback', function(req,res){
+        res.sendFile(__dirname + "/views/feedback.html");
+    });
+
+    router.get('/chair', function(req,res){
+        res.sendFile(__dirname + "/views/chair.html");
+    });
+
+    //backend GET API
+    router.get('/api/account/:uid', function (req, res){
+        var id = req.params.uid;
+        db.getAccount(uid, function(err, account){
+            if(err){
+                res.status(500).send("Unable to find user at this moment");
+            }else{
+                res.status(200).send(account);
+            }
+        })
+    });
+
+    router.get('/api/data/:uid', function (req, res){
+        var id = req.params.uid;
+        db.getRecords(function(err, data){
+            if(err){
+                res.status(500).send("Unable to get records at this moment");
+            }else{
+                res.status(200).send(data);
+            }
+        });
+    });
+
+    router.get('/api/settings/:uid', function(req,res){
+        var id = req.params.uid;
+        db.getSettings(id, function(err, settings){
+            if(err){
+                res.status(500).send("Unable to find user settings at this moment");
+            }else{
+                res.status(200).send(settings);
+            }
+        })
+    });
+
+    router.get('/api/setting/:uid/:sid', function(req, res){
+        var uid = req.params.uid;
+        var sid = req.params.sid;
+        db.getSetting(sid, uid, function(err, setting){
+            if(err){
+                res.status(500).send("Unable to get this setting at this moment");
+            }else{
+                res.status(200).send(settings);
+            }
+        })
+    });
+
+    //backend POST API
+    router.post('/api/registration', function(req,res){
+        db.addAccount(req.body.email, req.body.username, req.body.password, function(err, account){
+            if(err){
+                res.status(500).send("unable to add user now");
+            }else{
+                res.status(200).send(account);
+            }
+        });
+    });
+    
+    router.post('/api/record', function(req,res){
+        db.addRecord(req.body.userId, req.body.duration, req.body.date, req.body.postureCount, function(err,record){
+            if(err){
+                res.status(500).send("unable to record now");
+            }else{
+                res.status(200).send(record);
+            }
+        });
+    });
+
+    router.post('/api/setting', function(req, res){
+        db.addSetting(req.body.userId, req.body.status, req.body.strength, function(err, setting){
+            if(err){
+                res.status(500).send("unable to add setting now");
+            }else{
+                res.status(200).send(setting);
+            }
+        });
+    });
+
+    //backend PUT API
+
+    router.put('/api/setting/:uid', function(req, res){
+        var uid = req.params.uid;
+        var settingData = req.body;
+
+        db.updateSetting(uid, settingData.strength, settingData.status, function(err, setting){
+            if(err){
+                res.status(500).send("unable to update setting now");
+            }else{
+                res.status(200).send(setting);
+            }
+        })
+    });
+
+    //backend DELETE API
+
+    router.delete('/api/setting/:sid', function(req,res){
+        var uid = req.params.uid;
+
+        db.deleteSetting(uid, function(err, setting){
+            if (err) {
+                res.status(500).send("Unable to delete this setting");
+            } else {
+                res.status(200).send("setting");
+                
+            }
+        })
+    });
+
+    return router;
 };
 
 module.exports = routes();
--- Di-SmartChair_WebApi/services/dataservices.js
+++ Di-SmartChair_WebApi/services/dataservices.js
@@ -15,7 +15,7 @@ mongoose.set('useUnifiedTopology', true);
 var database = {
     connect: function(){
         mongoose.connect('mongodb://localhost:27017/DiSmartChairDB', function(err){
-            if(err = null){
+            if(err == null){
                 console.log("Connected to Mongo DB");
                 //Initialised Values
                 recordSchema = schema({
@@ -25,7 +25,6 @@ var database = {
                     postureCount : Number,
                 });
                 userSchema = schema({
-                    userId: String,
                     email : String,
                     username: String,
                     password : String,
@@ -38,18 +37,18 @@ var database = {
                 var connection = mongoose.connection;
                 recordModel = connection.model("Record", recordSchema);
                 userModel = connection.model("User", userSchema);
-                userModel = connection.model("Setting", settingSchema);
+                settingModel = connection.model("Setting", settingSchema);
             }else{
+                console.log(err);
                 console.log("Error connecting to Mongo DB");
             }
         })
     },
-    addAccount: function(uid, em, un,pw,callback){
+    addAccount: function(em, un,pw,callback){
         var newUser = new userModel({
-            userId = uid,
-            email = em,
-            username = un,
-            password = pw
+            email : em,
+            username : un,
+            password : pw
         });
         newUser.save(callback);
     },
@@ -58,10 +57,10 @@ var database = {
     },
     addRecord: function(uid, dura,Tdate, postC){
         var newRecord = new recordModel({
-            userId = uid,
-            duration = dura,
-            date = Tdate,
-            postureCount = postC,
+            userId : uid,
+            duration : dura,
+            date : Tdate,
+            postureCount : postC,
         });
     },
     getRecords: function(callback){
@@ -69,20 +68,23 @@ var database = {
     },
     addSetting: function(uid, st, str,callback){
         var newSetting = new settingModel({
-            userid = uid,
-            status = st,
-            strength = str
+            userid : uid,
+            status : st,
+            strength : str
         });
         settingModel.save(callback);
     },
+    getSettings: function(uid, callback){
+        settingModel.find({userid : uid});
+    },
     getSetting: function(sid, uid, callback){
-        settingModel.find({_id = sid , userid = uid}, callback);
+        settingModel.find({_id : sid , userid : uid}, callback);
     },
     updateSetting: function(sid,str,stats, callback){
-        settingModel.updateOne({_id = sid}, {strength = str, status = stats}, callback);
+        settingModel.updateOne({_id : sid}, {strength : str, status : stats}, callback);
     },
     deleteSetting: function(sid, callback){
-        settingModel.deleteOne({_id = sid}, callback)
+        settingModel.deleteOne({_id : sid}, callback)
     },
 };
 
