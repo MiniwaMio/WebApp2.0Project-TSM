58a67390f25cef0e95b3352d1bf1a754d4f2518c
ekolmas <66930465+ekolmas@users.noreply.github.com>
2020-07-11 14:13:21 +0800

    Update registration.js

--- Di-SmartChair_WebApi/views/js/registration.js
+++ Di-SmartChair_WebApi/views/js/registration.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
         ).done(
             function(data){
                 //console log input
+                
                 console.log(data);
             }
         ).fail(
