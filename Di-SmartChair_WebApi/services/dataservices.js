1e2f4c5fc3c4c05c1b7eb2194f5b72f0ac1f527d
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2020-07-24 23:07:06 +0800

    Revert "Revert "Revert "implemented authentication to login"""
    This reverts commit 42a6c4e1fa02c594b227cfca34ba7a109006ad8c.

--- Di-SmartChair_WebApi/services/dataservices.js
+++ Di-SmartChair_WebApi/services/dataservices.js
@@ -29,11 +29,16 @@ var database = {
                     email : String,
                     username: String,
                     password : String,
-                    token: String
+                });
+                settingSchema = schema({
+                    userId: String,
+                    status: String,
+                    strength : Number,
                 });
                 var connection = mongoose.connection;
                 recordModel = connection.model("Record", recordSchema);
                 userModel = connection.model("User", userSchema);
+                settingModel = connection.model("Setting", settingSchema);
             }else{
                 console.log(err);
                 console.log("Error connecting to Mongo DB");
@@ -51,12 +56,6 @@ var database = {
     getAccount: function(id, callback){
         userModel.findById(id, callback);
     },
-    updateToken: function (id, token, callback) {
-        userModel.findByIdAndUpdate(id, { token: token }, callback);
-    },
-    checkToken: function(token,callback) {
-        userModel.findOne({token:token},callback);
-    },
     findIfExisting: function(uname, pword, callback){
         userModel.findOne({username : uname, password : pword}, callback)
     },
@@ -72,8 +71,30 @@ var database = {
     getRecords: function(callback){
         recordModel.find({}, callback);
     },
+    addSetting: function(uid, st, str,callback){
+        var newSetting = new settingModel({
+            userid : uid,
+            status : st,
+            strength : str
+        });
+        newSetting.save(callback);
+    },
+    getSettings: function(uid, callback){
+        settingModel.find({userId : uid}, callback);
+    },
+    getSetting: function(sid, uid, callback){
+        settingModel.find({_id : ObjectId(sid) , userId : uid}, callback);
+    },
+    updateSetting: function(sid,str,stats, callback){
+        settingModel.updateOne({_id : ObjectId(sid)}, {strength : str, status : stats}, callback);
+    },
+    deleteSetting: function(sid, callback){
+        settingModel.deleteOne({_id : ObjectId(sid)}, callback)
+    },
 
-    
+    getName: function(uid,callback){
+        userModel
+    }
 };
 
 module.exports = database;
--- Di-SmartChair_WebApi/views/js/index.js
+++ Di-SmartChair_WebApi/views/js/index.js
@@ -1,30 +1,26 @@
 //post the fields to check for login
-
 $(document).ready(function(){
-    var token = sessionStorage.authToken;
-});
-//validate
-function login() {
-    var credentials = {
-        username: $('#username').val(),
-        password: $('#password').val()
-        
-    }
-    $.ajax({
-        url:'/login',
-        method:'post',
-        data:credentials
-    })
-    .done(function(data){
-        console.log(data);
-        $('.statusMessage').text(data.message);
-  //stores the token returned from the server, if successful login
-        sessionStorage.authToken=data.token; 
-        sessionStorage.setItem('userId', data.uid);
-    })
-    .fail(function(err){
-        $('.statusMessage').text(err.responseText);
-    })
 
-    return false;
-}
+    $("#submit").click(function(){
+        var loginDetails = {
+            username: $('#username').val(),
+            password: $('#password').val(),
+        };
+        $.ajax({
+    
+            url: '/login',
+            method: 'post',
+            data: loginDetails,
+        }
+        ).done(
+            function(data){
+                //console log input
+                console.log(data);
+            }
+        ).fail(
+            function(err){
+                console.log(err.responseText);
+            }
+        );
+    });
+});
