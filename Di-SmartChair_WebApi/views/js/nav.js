6c58c37ec4f8b5cf8c0609b4de8d65fcbd22f1c2
ekolmas <66930465+ekolmas@users.noreply.github.com>
2020-08-02 16:06:55 +0800

    Update nav.js

--- Di-SmartChair_WebApi/views/js/nav.js
+++ Di-SmartChair_WebApi/views/js/nav.js
@@ -1,13 +1,13 @@
 var userId=0;
 $(document).ready(function(){
  userId = sessionStorage.userId;
- $('.blue').append("<a href='/api/chair?id="+userId+"&token="+ sessionStorage.authToken +"'> CHAIR</a>")
- $('.blue').append("<a href='/api/data?id="+userId+"&token="+ sessionStorage.authToken +"'> DATA</a>")
- $('.blue').append("<a href='/api/features?id="+userId+"&token="+ sessionStorage.authToken +"'> FEATURES</a>")
- $('.blue').append("<a href='/api/feedback?id="+userId+"&token="+ sessionStorage.authToken +"'> FEEDBACK</a>")
+ $('.blue').append("<a href='/api/chair/id="+userId+"&token="+ sessionStorage.authToken +"'> CHAIR</a>")
+ $('.blue').append("<a href='/api/data/id="+userId+"&token="+ sessionStorage.authToken +"'> DATA</a>")
+ $('.blue').append("<a href='/api/features/id="+userId+"&token="+ sessionStorage.authToken +"'> FEATURES</a>")
+ $('.blue').append("<a href='/api/feedback/id="+userId+"&token="+ sessionStorage.authToken +"'> FEEDBACK</a>")
 
- $('.databutton').wrap("<a href='/api/data?id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
- $('.sittingbutton').wrap("<a href='/api/features?id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
- $('.feedbackbutton').wrap("<a href='/api/feedback?id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
- $('.logoutbutton').wrap("<a href='/api/logout?id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
+ $('.databutton').wrap("<a href='/api/data/id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
+ $('.sittingbutton').wrap("<a href='/api/features/id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
+ $('.feedbackbutton').wrap("<a href='/api/feedback/id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
+ $('.logoutbutton').wrap("<a href='/api/logout/id="+userId+"&token="+ sessionStorage.authToken +"'></a>");
 });
