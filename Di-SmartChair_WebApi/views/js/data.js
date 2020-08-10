a9e675edeace2a451d0fbc96c8d90813e072a97d
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2020-08-10 15:48:55 +0800

    Added prep file for displaying data
    1. Added canvas on data.html
    2. Added script file for Chart.js
    3. Changed format for registration page

--- Di-SmartChair_WebApi/views/js/data.js
+++ Di-SmartChair_WebApi/views/js/data.js
@@ -2,7 +2,7 @@
 var userId = 0;
 $(document).ready(function(){
     userId = sessionStorage.getItem("userId");
-
+    
     $.ajax({
         url: "/api/data/"+userId+ "?token=" + sessionStorage.authToken,
         method:"get"
@@ -25,6 +25,22 @@ $(document).ready(function(){
             console.log(err.responseText);
         }
     );
+
+    //sets data to canvas
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:'rgba(255, 99, 132, 0.2)',
+                borderColor:'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+    });
 
     //username
 //sitting duration
